80bb55cb03a0b3494e578c97feb5a519a6528e6f
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-01-16 23:37:42 +0900

    fix#5: GNB 로고 이미지 반응형 기준 수정

--- src/components/common/GNB.tsx
+++ src/components/common/GNB.tsx
@@ -8,7 +8,7 @@ export function GNB() {
   return (
     <header className="w-full h-[60px] flex flex-row justify-start items-center pl-[24px] lg:pl-[360px]">
       <picture>
-        <source media="(min-width: 768px)" srcSet="/icons/logo-large.svg" />
+        <source media="(min-width: 640px)" srcSet="/icons/logo-large.svg" />
         <img
           src="/icons/logo-small.svg"
           alt="logo"
